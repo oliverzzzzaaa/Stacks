aaa2c8aca3443795edd6e6c4ac36e0ab06219f91
Oliver <oliverzzzzaaa@gmail.com>
2020-01-11 21:13:19 -0800

    Added texteditor without submit

--- frontend/components/main/chat.jsx
+++ frontend/components/main/chat.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import Quill from 'quill'
 
 class Chat extends React.Component {
     constructor(props) {
         super(props)
         // this.state = this.props.messages
         // this.bottom = React.createRef();
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
         this.props.fetchMessages();
         this.props.fetchChannels();
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+          });
+        const form = document.querySelector('form');
+        form.onsubmit = function() {
+        var body = document.querySelector('input[name=body]');
+        body.value = JSON.stringify(quill.getContents());
+        console.log("Submitted", $(form).serialize(), $(form).serializeArray());
+        };
+          
         // App.cable.subscriptions.create(
         //     { channel: "ChatChannel" },
         //     {
@@ -27,10 +39,13 @@ class Chat extends React.Component {
     //     this.bottom.current.scrollIntoView();
     // }
 
+        handleSubmit() {
+            console.log("great!")
+            
+        }
+
     render() {
-        // const messageList = (
-        //     <div>MessageList</div>
-        // );
+          
         const messageList = this.props.messages.map( message => {
             return (
                 <li key={message.id} className="message-li">
@@ -54,17 +69,27 @@ class Chat extends React.Component {
                 </li>
             )
         })
+
         return (
+
+            
             <div className="chat-container">
                 <div id="chat-header">
                     <div id="chat-container-left">
-
                     </div>
                 </div>
                 <div className="message-list">
                     {messageList}
                 </div> 
                 <div id="create-message-div">
+                    <form onSubmit={this.handleSubmit}>
+                        <div id="editor">
+                            <p></p>
+                            <p></p>
+                            <p><br/></p>
+                        </div>
+                        <input type="submit" hidden={true}/>
+                    </form>
                 </div>
             </div>
         )
